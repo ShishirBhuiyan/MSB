f96d91d0fa256765ca8e04bdbc13ac152a9aa18a
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-05-15 23:35:06 +0600

    Profile

--- Asset/app.js
+++ Asset/app.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
 
+    //<===========Animate on Scroll =============>
+    AOS.init();
+
+
     //<=========== Hamburger Click =============>
     $(".toggle").click(function() {
         $(".toggle .line").toggleClass("active");
          $("header").toggleClass("open");
     });
 
+    //<=========== Ropples Effect =============>
+    $(".profile").ripples({
+        resolation: 512,
+        dropRadius: 10
+    });
+    $(".footer").ripples({
+        resolation: 512,
+        dropRadius: 10
+    });
+    $("#body").ripples({
+        resolation: 512,
+        dropRadius: 10
+    });
 
 
 });
