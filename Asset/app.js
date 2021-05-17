8646c522525743b0deca821a9850e823e89fead0
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-05-17 22:52:41 +0600

    Education & Skill

--- Asset/app.js
+++ Asset/app.js
@@ -41,3 +41,28 @@ function ReadMore() {
     $(".pop-overlay").toggleClass("openoverlay");
     document.getElementById("popup").classList.toggle("active");
 }
+
+
+
+//<=========== Counter Up Animation =============>
+const tooltips = document.querySelectorAll(".tooltips");
+function runprogress() {
+tooltips.forEach((o) => {
+   o.innerText = 0;
+   var e = +o.dataset.percent;
+   var t = Math.ceil(e / 50);
+   var n = function() {
+       var l = +o.innerText;
+       l < e ? ((o.innerText = l + t), (o.parentElement.style.width = o.innerText + "%"), setTimeout(n, 50)) : (o.innerText = +e + "%");
+   };
+   n();
+});
+}
+var footerSection = document.querySelector("footer"),
+options = {
+rootMargin: "0px 0px 200px 0px"
+};
+const sectionObserver = new IntersectionObserver(function(o) {
+runprogress();
+}, options);
+sectionObserver.observe(footerSection);
